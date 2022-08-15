8d6be22a7b606f2a56fa6a8d5b44551bcd211a87
Francelino <fmarcilio1989@gmail.com>
2022-08-15 11:57:33 -0300

    retirando código pronto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { LayoutModule } from '@angular/cdk/layout';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MAT_DATE_LOCALE } from '@angular/material/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { LayoutModule } from "@angular/cdk/layout";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MAT_DATE_LOCALE } from "@angular/material/core";
+import { HttpClientModule } from "@angular/common/http";
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing.module';
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app.routing.module";
 
-import { MaterialModule } from './shared/material/material.module';
-import { TopoComponent } from './shared/components/topo/topo.component';
-import { RodapeComponent } from './shared/components/rodape/rodape.component';
-import { AlertaComponent } from './shared/components/alerta/alerta.component';
-
+import { MaterialModule } from "./shared/material/material.module";
+import { TopoComponent } from "./shared/components/topo/topo.component";
+import { RodapeComponent } from "./shared/components/rodape/rodape.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopoComponent,
-    RodapeComponent,
-    AlertaComponent,
-  ],
+  declarations: [AppComponent, TopoComponent, RodapeComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     LayoutModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
-  entryComponents: [AlertaComponent],
-  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt' }],
-  bootstrap: [AppComponent]
+  entryComponents: [],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: "pt" }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

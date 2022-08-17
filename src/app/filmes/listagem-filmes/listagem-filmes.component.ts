acd9b733038586487fa0fd7ebfa0bb732885bf35
Francelino <fmarcilio1989@gmail.com>
2022-08-17 14:23:41 -0300

    feat campos de pesquisa na listagem

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
 import { FilmesService } from "src/app/core/filmes.service";
 import { Filme } from "src/app/shared/models/filme";
 
@@ -8,12 +9,28 @@ import { Filme } from "src/app/shared/models/filme";
   styleUrls: ["./listagem-filmes.component.scss"],
 })
 export class ListagemFilmesComponent implements OnInit {
+  filtrosListagem: FormGroup;
   filmes: Filme[] = [];
   pagina = 0;
   readonly qtdPagina = 4;
-  constructor(private filmesService: FilmesService) {}
+  generos: Array<string>;
+
+  constructor(private filmesService: FilmesService, private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.filtrosListagem = this.fb.group({
+      texto: [""],
+      genero: [""],
+    });
+    this.generos = [
+      "Ação",
+      "Romance",
+      "Aventura",
+      "Terror",
+      "Ficção Cientifica",
+      "Comédia",
+      "Drama",
+    ];
     this.listarFilmes();
   }
 

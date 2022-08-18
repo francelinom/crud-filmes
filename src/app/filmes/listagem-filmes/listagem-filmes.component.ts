df50db367a0149c6d7a5285866a65f54793d6e6d
Francelino <fmarcilio1989@gmail.com>
2022-08-18 13:12:52 -0300

    feat utilizando o debounceTime para que faça uma requisição apos o tempo estabelicido
    adicionando classe para mensagem de quando não tem filmes

--- src/app/filmes/listagem-filmes/listagem-filmes.component.ts
+++ src/app/filmes/listagem-filmes/listagem-filmes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup } from "@angular/forms";
+import { debounceTime } from "rxjs/operators";
 import { FilmesService } from "src/app/core/filmes.service";
 import { ConfigParams } from "src/app/shared/models/config-params";
 import { Filme } from "src/app/shared/models/filme";
@@ -10,6 +11,9 @@ import { Filme } from "src/app/shared/models/filme";
   styleUrls: ["./listagem-filmes.component.scss"],
 })
 export class ListagemFilmesComponent implements OnInit {
+  readonly semFoto =
+    "https://termoparts.com.br/wp-content/uploads/2017/10/no-image.jpg";
+
   filtrosListagem: FormGroup;
   filmes: Filme[] = [];
   generos: Array<string>;
@@ -26,10 +30,13 @@ export class ListagemFilmesComponent implements OnInit {
       genero: [""],
     });
 
-    this.filtrosListagem.get("texto").valueChanges.subscribe((val: string) => {
-      this.config.pesquisa = val;
-      this.resetarConsulta();
-    });
+    this.filtrosListagem
+      .get("texto")
+      .valueChanges.pipe(debounceTime(400))
+      .subscribe((val: string) => {
+        this.config.pesquisa = val;
+        this.resetarConsulta();
+      });
 
     this.filtrosListagem.get("genero").valueChanges.subscribe((val: string) => {
       this.config.campo = { tipo: "genero", valor: val };
